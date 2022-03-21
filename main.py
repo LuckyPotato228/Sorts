3f424e01c2ad37d09a29e5582df12e06d4dee67c
LuckyPotato228 <91941548+LuckyPotato228@users.noreply.github.com>
2022-03-21 10:11:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 def select_sort():
     n = int(input())
     arr = [int(i) for i in input().split()]
-    b = []
+    b = arr[::1]
     for i in range(n+1):
-        b = arr
-        b.insert(i, "|")
+        try:
+            arr.insert(i,'|')
+            print(*arr)
+            arr.pop(i)
+            if i > 0:
+                b.pop(0)
+            index = b.index(min(b))
+            b[0],b[index] = b[index],b[0]
+            arr[i:] = b
+        except ValueError:
+            break
+
+
+def insertion_sort():
+    n = int(input())
+    arr = [int(i) for i in input().split()]
+    b = arr[::1]
+    for i in range(n + 1):
+        b = list(map(str,arr))
+        index = arr.index(min(arr))
+        num = str(arr[index])
+        b[index] = str('<' + num + '>')
+        b.insert(i,'|')
         print(b)
-        b.pop(i)
-        for i in range(len(arr)):
-            pass
-select_sort()
+
+
+
+insertion_sort()
